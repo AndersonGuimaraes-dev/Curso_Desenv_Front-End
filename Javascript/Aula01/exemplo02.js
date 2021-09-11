9722013de468c7c258884002288703ef09e03f88
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-11 11:08:46 -0300

    Bloco de codigos
    Trabalhando com blocos de códigos

---
+++ Javascript/Aula01/exemplo02.js
@@ -0,0 +1,15 @@
+{
+    console.log("teste")
+}
+
+{
+    console.log("teste 2")
+}
+/* O que fica entre as chaves, é chamdo de bloco de código */
+/* Pode haver bloco de código detro de um outro bloco de código */
+{
+    {
+        console.log("teste 3")
+    }
+    console.log("teste 4")
+}
