49dc2739ba8134728637385e6acb49bef9d38ea7
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-11 11:04:56 -0300

    Comecando com js
    Comando console.log; comentários e quebra de linha

---
+++ Javascript/Aula01/exemplo01.js
@@ -0,0 +1,12 @@
+console.log('Olá, mundo!'); // O comando console.log imprime o resultado na tela
+// Para inserir comentaráios, podemos usar duas barras para comentários de uma linha ou barra asterisco asterisco barra para comentários de mais de uma linha
+/*
+*comentário
+* de múltiplas
+*linhas
+*/
+console.log('Olá, teste');
+//console.log(' \n '); - insere uma quebra de linha
+console.log('Primeira linha');
+console.log("\n");
+console.log("Segunda linha");
