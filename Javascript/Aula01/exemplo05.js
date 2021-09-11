7db1a9c9d6f552cb60e660824dabfb1bc6d8aa73
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-11 12:09:29 -0300

    Tipos de dados
    Como saber qual o tipo de dado inserido por meio do "typeof" - console.log(typeof )

---
+++ Javascript/Aula01/exemplo05.js
@@ -0,0 +1,11 @@
+// Descobrindo o tipo de dados
+let idade = 30;
+let nome = "Anderson";
+let salario = 1000.50;
+
+console.log(typeof idade);// pode colocar o nome da variável ou o valor da variável: console.log(typeof 30)
+console.log(typeof nome);
+
+// Variáveis booleanas
+let aula_comecou = true; // só podem receber dois valores ou "true" ou "false"
+console.log(typeof aula_comecou);
