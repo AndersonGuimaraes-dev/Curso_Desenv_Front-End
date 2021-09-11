b470df4cf2ad4144f29c5fc750818db8ca2223ca
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-11 11:51:08 -0300

    Aula assincrona
    Aula assíncrona do dia 09 de setembro JS

---
+++ Javascript/Exercicios de Javascript/Aulas assíncronas/mediaJoao.js
@@ -0,0 +1,10 @@
+// Aula assíncrona 09 set
+// Cálculo da média das notas de João
+let nome = "João"
+let nota1 = 8;
+let nota2 = 4;
+let nota3 = 9;
+let media;
+media = (nota1+nota2+nota3)/3;
+console.log(nome+", nota final = "+media);
+
