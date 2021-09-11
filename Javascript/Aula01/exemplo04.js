903a6f3cf33cdaf7fd910f6951f45b0ec3f115e5
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-11 11:55:07 -0300

    Operadores aritimeticos
    Trabalhando com operadores aritméticos

---
+++ Javascript/Aula01/exemplo04.js
@@ -0,0 +1,16 @@
+// Operadores aritméticos (soma, subtração, multiplicação e divisão)
+let numero1 = 10;
+let numero2 = 20;
+
+let soma;//criando uma nova variavel, mas sem declarar o valor
+soma = numero1+numero2;//informa o valor de "soma"
+
+console.log(soma)//mostra o valor da variável "soma"
+console.log(" \n ")
+
+let km_rodados = 30;
+let litros = 6;
+
+let media_consumo;
+media_consumo = km_rodados / litros;
+console.log(media_consumo);
