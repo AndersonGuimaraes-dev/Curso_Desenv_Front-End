ab82668ffa90eb94713ec1390ff6367694aa9248
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-11 11:26:47 -0300

    Variaveis
    Trabalhando com variáveis

---
+++ Javascript/Aula01/exemplo03.js
@@ -0,0 +1,19 @@
+/* Variáveis - nome e valor (usa-se o var ou o let para declarar variáveis*/
+    // Variável do tipo double (número real)
+    let preco = 6.17;
+    // Variável do tipo int (inteiro)
+    let idade = 18;
+    // Variável do tipo string (caracter) - entre aspas simples ou duplas
+    let nome = "Anderson";
+    // para exibir na tela
+    {
+        console.log(nome);
+    }
+    // Para mostrar resultado com mais de uma variável
+    {
+        console.log(nome+" "+idade); //colacando um espaço em branco
+    }
+    {
+        preco = 6.05 //alteração o valor da variável preco
+        console.log(preco);
+    }
