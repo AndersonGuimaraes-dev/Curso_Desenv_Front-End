fbb5390ebffabc808a3e6349ca319cdddf7e0520
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-11 11:11:42 -0300

    Variaveis
    Sobre  não ser necessário informar o tipo de variável

--- Javascript/Aula01/exemplo02.js
+++ Javascript/Aula01/exemplo02.js
@@ -1,15 +1,17 @@
 {
-    console.log("teste")
+    console.log("teste");
 }
 
 {
-    console.log("teste 2")
+    console.log("teste 2");
 }
 /* O que fica entre as chaves, é chamdo de bloco de código */
 /* Pode haver bloco de código detro de um outro bloco de código */
 {
     {
-        console.log("teste 3")
+        console.log("teste 3");
     }
-    console.log("teste 4")
+    console.log("teste 4");
 }
+console.log(2);
+/* No JS não há necessidade de declarar qual o tipo de variável: caracter, string, numero, booleano etc. */
