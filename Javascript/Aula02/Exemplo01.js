676c7b6af9bb95a58c4a4d5e6e0783b0966f08ab
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-11 13:05:50 -0300

    Peculiaridades
    Peculiaridades JS, como por exemplo, mudar o tipo e o valor das variáveis, e tratar valores como booleanos verdadeiros ou falsos.

---
+++ Javascript/Aula02/Exemplo01.js
@@ -0,0 +1,10 @@
+let numero = 6;//em javascript apenas inserir o comando let e o nome da variável e atribuir o valor
+console.log(typeof numero);
+numero = "Anderson" //mudança de tipo de dado de número para caracter
+console.log(typeof numero);// no JS pode ser mudado o valor da variável bem como o tipo da variável
+console.log(8/0);// Não gera erro, mas gera um resultado "infinity"
+ativo = true; 
+console.log(typeof ativo);
+ativo2 = 1;
+console.log(!!ativo2);//!! trata a variável com booleana e traz o vaor como verdadeiro, se usar apenas 1 !, o valor será falso
+console.log(!ativo2);//valor falso
