31796e8fd40d6f393f540076a92e3d53b8f60730
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-13 20:35:02 -0300

    Exemplo de if
    Exemplo de código apenas com if em vez do else if

---
+++ Javascript/Aula03/If.js
@@ -0,0 +1,16 @@
+let nome = "José";
+let cargo;
+
+if (nome == "Pedro"){
+    cargo = "Professor" 
+}if(nome == "João"){
+    cargo = "Engenheiro"
+}if(nome == "Ana"){
+    cargo = "Professora"
+}if(nome == "Anderson"){
+    cargo = "Músico"
+
+}
+
+console.log(nome)
+console.log(cargo)
