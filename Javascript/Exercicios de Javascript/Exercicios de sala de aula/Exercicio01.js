67a16daa508ae32bb37b5867db19b70e76162a07
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-13 20:36:51 -0300

    Exercicio de if
    Criar um código para determinar se o número é par ou ímpar

---
+++ Javascript/Exercicios de Javascript/Exercicios de sala de aula/Exercicio01.js
@@ -0,0 +1,12 @@
+// Informe um número e verifique se o número informado é par ou ímpar
+// % resto da divisão "/" resultado da divisão
+
+function recebeNumero(numero){
+    if(numero % 2 == 0){
+    console.log("O número é par")
+}else{
+    console.log("O número é ímpar")
+}
+}
+
+recebeNumero(22)
