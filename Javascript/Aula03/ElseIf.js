85d67a417cde1c931f241ed673829400044c5c61
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-13 20:33:59 -0300

    Else if
    Uso do else if...

---
+++ Javascript/Aula03/ElseIf.js
@@ -0,0 +1,20 @@
+// If (se); else(senão); else if (senão se)
+
+let nome = "Anderson";
+let cargo;
+
+if (nome == "Pedro"){
+    cargo = "Professor" 
+}else if(nome == "João"){
+    cargo = "Engenheiro"
+}else if(nome == "Ana"){
+    cargo = "Professora"
+}else if(nome == "Anderson"){
+    cargo = "Músico"
+}else{
+    cargo = "Não informado"
+}
+
+console.log(nome)
+console.log(cargo)
+
