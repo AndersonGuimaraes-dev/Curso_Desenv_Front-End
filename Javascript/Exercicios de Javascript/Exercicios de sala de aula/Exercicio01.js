24e0be1745b34e8e7fb7648f803b6d04b8d5496d
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-14 13:18:52 -0300

    Array
    Exercícios sobre Array

---
+++ Javascript/Exercicios de Javascript/Exercicios de sala de aula/Exercicio01.js
@@ -0,0 +1,10 @@
+/*
+Exercício sobre array
+Inicie com o array preco, altere o preço do 1, 3 e 7 elementos do array.
+*/
+const preco = [1.23, 56.78, 45.89, 34.23, 56.15, 18.09, 7.10,  78.90]
+console.log(preco)//imprime os dados do array
+preco[1] = 56.87
+preco[3] = 34.32
+preco[7] =78.09
+console.log(preco)//imprime os valores do array com as alterações
