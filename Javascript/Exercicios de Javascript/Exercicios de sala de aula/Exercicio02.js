e51a6ebd44b305505af705b544a684750d1814d8
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-14 13:58:38 -0300

    Exercicio Funcao
    Exercício sobre função de transformar de
    Celsius para Fahrenheit

---
+++ Javascript/Exercicios de Javascript/Exercicios de sala de aula/Exercicio02.js
@@ -0,0 +1,8 @@
+//Exercício sobre Funções
+//Crie uma função que retorne o valor convertido de uma temperatura em celsius para Fahrenheit
+
+function temp(c){
+    let Fahrenheit = c * 1.8 + 32
+    console.log(Fahrenheit)
+}
+temp(25)
