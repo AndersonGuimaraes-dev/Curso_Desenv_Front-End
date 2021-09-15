bd31add39332c7000184032dec5aa4b054208483
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-14 21:59:50 -0300

    ForEach
    Uso do FOR EACH com array

---
+++ Javascript/Aula04/ForEach.js
@@ -0,0 +1,6 @@
+// Uso da propriedade For Each
+
+var marcas = ["FIAT", "FORD", "BMW","FERRARI", "VW", "TOYOTA"];
+marcas.forEach((marca, indice) => {
+    console.log("Marca: "+marca+" Índice: "+indice);
+})
