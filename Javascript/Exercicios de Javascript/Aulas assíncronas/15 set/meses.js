5a98022baf5645507da6bed8a3b2f1928449b2dd
Anderson de Matos Guimarães <andersonmguimaraes@icloud.com>
2021-09-16 19:23:00 -0300

    Meses
    Script para inserir um número e ser retornado o mês correspondente

---
+++ Javascript/Exercicios de Javascript/Aulas assíncronas/15 set/meses.js
@@ -0,0 +1,36 @@
+/*
+Crie uma função que receba um parâmetro (de 1 a 12) e retorne o mês correspondente como uma string. Exemplo valor informado 1 retorno Janeiro, valor informado 2 retorno Fevereiro...
+*/
+let mes = function (numeroMes){
+    
+    switch(numeroMes){
+
+        case 1: console.log('Janeiro');
+        break
+        case 2: console.log('Fevereiro');
+        break
+        case 3: console.log('Março');
+        break
+        case 4: console.log('Abril');
+        break
+        case 5: console.log('Maio');
+        break
+        case 6: console.log('Junho');
+        break
+        case 7: console.log('Julho');
+        break
+        case 8: console.log('Agosto');
+        break
+        case 9: console.log('Setembro');
+        break
+        case 10: console.log('Outubro');
+        break
+        case 11: console.log('Novembro');
+        break
+        case 12: console.log('Dezembro');
+        break
+
+    }
+
+}
+mes(1)
